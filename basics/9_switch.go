8d9b900c098e56992bf54e880f8c09ad5516b574
shevchenkodim <dimashevchenko10021999@gmail.com>
2021-08-12 18:59:17 +0300

    Switch in go

---
+++ basics/9_switch.go
@@ -0,0 +1,50 @@
+package basics
+
+/*
+   switch time.Now().Weekday() {
+
+   case time.Monday:
+       fmt.Println("Today is Monday")
+
+   case time.Tuesday:
+       fmt.Println("Today is Tuesday")
+
+   case time.Wednesday:
+       fmt.Println("Today is Wednesday")
+
+   case time.Thursday:
+       fmt.Println("Today is Thursday")
+
+   case time.Friday:
+       fmt.Println("Today is Friday")
+
+   case time.Saturday:
+       fmt.Println("Today is Saturday")
+
+   case time.Sunday:
+       fmt.Println("Today is Sunday")
+   }
+*/
+
+/*
+	switch time.Now().Weekday() {
+
+	case time.Monday, time.Tuesday, time.Wednesday, time.Thursday, time.Friday:
+		fmt.Println("weekday")
+	case time.Saturday, time.Sunday:
+		fmt.Println("weekend")
+	}
+*/
+
+/*
+	var data interface {}
+
+	data = 23423.234
+
+	switch myType := data.(type) {
+	case string:
+		fmt.Println("string")
+	default:
+		fmt.Println("default", myType)
+	}
+*/
