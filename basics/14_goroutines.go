4dddd928928704f1523355c9d0ea3c84cf4337cb
shevchenkodim <dimashevchenko10021999@gmail.com>
2021-08-16 17:05:03 +0300

    Goroutines in Go

---
+++ basics/14_goroutines.go
@@ -0,0 +1,36 @@
+package basics
+
+/*
+	intCh := make(chan int, 3)
+	intCh <- 10
+	intCh <- 1
+
+	fmt.Println(cap(intCh))
+	fmt.Println(len(intCh))
+
+	fmt.Println(<-intCh)
+	fmt.Println(<-intCh)
+*/
+
+/*
+	intChannelTest := make(chan map[string]int)
+
+	go func(){
+		fmt.Println("Go routine starts")
+		intChannelTest <- map[string]int {"test": 6, "test2": 12}
+	}()
+
+	fmt.Println((<- intChannelTest)["test"])
+*/
+
+/*
+	fmt.Println(<- createChan(5))
+
+	func createChan(n int) chan int {
+		ch := make(chan int)
+		go func() {
+			ch <- n
+		}()
+		return ch
+	}
+*/
